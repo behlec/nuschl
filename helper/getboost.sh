29a108b205d9a3a3cd8ea0a4643e46cfecd2707a
Christoph Behle <behlec@gmail.com>
2017-12-17 18:54:02 +0100

    More output

--- helper/getboost.sh
+++ helper/getboost.sh
@@ -5,6 +5,7 @@ if [ -e download/boost_1_59_0.tar.gz ]; then
   exit
 fi
 
+echo 'Downloading Boost;
 mkdir -p download
 cd download
 wget -O boost_1_59_0.tar.gz http://sourceforge.net/projects/boost/files/boost/1.59.0/boost_1_59_0.tar.gz/download
