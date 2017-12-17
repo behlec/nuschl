1225c714807acf62a9ed5898bbb18a64e42914b6
Christoph Behle <behlec@gmail.com>
2017-12-17 18:35:35 +0100

    Improve download script

--- helper/getboost.sh
+++ helper/getboost.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-if [ -d download ]; then
+if [ -e download/boost_1_59_0.tar.gz ]; then
+  echo 'Boost is already there'
   exit
 fi
 
-mkdir download
+mkdir -p download
 cd download
 wget -O boost_1_59_0.tar.gz http://sourceforge.net/projects/boost/files/boost/1.59.0/boost_1_59_0.tar.gz/download
