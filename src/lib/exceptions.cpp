cf4b479584ad54f03cc25db64eeaba947956cced
Christoph Behle <behlec@gmail.com>
2018-01-06 18:36:08 +0100

    Unbound variable exception. Coverage.
    unbound_variable_error derives from runtime_error.
    Add tests.

--- src/lib/exceptions.cpp
+++ src/lib/exceptions.cpp
@@ -1,5 +1,9 @@
 #include <nuschl/exceptions.hpp>
 
+#include <string>
+
+using namespace std::string_literals;
+
 nuschl::eval_error::eval_error(const char *s, const s_exp_ptr e)
     : std::runtime_error(s), m_exp(e) {}
 
@@ -9,10 +13,11 @@ nuschl::eval_argument_error::eval_argument_error(const char *w)
     : std::runtime_error(w) {}
 
 nuschl::unbound_variable_error::unbound_variable_error(const std::string &name)
-    : m_name(name) {}
+    : std::runtime_error("Unbound variable: "s + name), m_name(name) {}
 
 nuschl::unbound_variable_error::unbound_variable_error(const std::string &&name)
-    : m_name(std::move(name)) {}
+    : std::runtime_error("Unbound variable: "s + name),
+      m_name(std::move(name)) {}
 
 const std::string &nuschl::unbound_variable_error::name() const {
     return m_name;
