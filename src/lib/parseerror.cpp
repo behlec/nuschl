ab1d1385d45b77553c73e59ed2552993988e8fbf
Christoph Behle <behlec@gmail.com>
2018-01-09 19:34:50 +0100

    Tests and fix for parse_error.

--- src/lib/parseerror.cpp
+++ src/lib/parseerror.cpp
@@ -2,11 +2,11 @@
 
 nuschl::parsing::parse_error::parse_error(const std::string &what,
                                           input_iterator pos)
-    : std::runtime_error(what) {}
+    : std::runtime_error(what), m_pos(pos) {}
 
 nuschl::parsing::parse_error::parse_error(std::string &&what,
                                           input_iterator pos)
-    : std::runtime_error(std::move(what)) {}
+    : std::runtime_error(std::move(what)), m_pos(pos) {}
 
 nuschl::parsing::input_iterator nuschl::parsing::parse_error::where() const {
     return m_pos;
