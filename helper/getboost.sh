5cd73279b801157e57adae7e97b1424da82371fc
Christoph Behle <behlec@gmail.com>
2018-03-02 21:22:24 +0100

    Change boost download.
    Download from github.

--- helper/getboost.sh
+++ helper/getboost.sh
@@ -8,4 +8,4 @@ fi
 echo 'Downloading Boost'
 mkdir -p download
 cd download
-wget -O boost_1_59_0.tar.gz http://sourceforge.net/projects/boost/files/boost/1.59.0/boost_1_59_0.tar.gz/download
+wget -O boost_1_59_0.tar.gz https://github.com/boostorg/boost/archive/boost-1.59.0.tar.gz
